1fb579ccb8a05acbbc464e54b10d7efb69cee876
Twosun8Git <cocominawork+twosun8git@gmail.com>
2024-10-20 13:21:47 +0900

    no message

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
+import { setStatusBarStyle } from "expo-status-bar";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("light");
+    }, 0);
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
